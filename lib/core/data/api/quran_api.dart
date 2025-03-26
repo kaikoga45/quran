a3c2c337c3384d0041a59486dd156d95966d6d1e
Kai Koga <koga.kai@icloud.com>
2025-03-26 14:20:56 +0700

    add quran api

---
+++ lib/core/data/api/quran_api.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:quran/core/domain/entities/detail_surah_entities.dart';
+import 'package:quran/core/domain/entities/surah_entities.dart';
+
+enum QuranApiEndpoint {
+  surah,
+}
+
+class QuranApi {
+  final Dio _dio;
+
+  QuranApi(Dio dio) : _dio = dio;
+
+  static const String _baseUrl = 'https://open-api.my.id/api/quran';
+
+  Future<List<SurahEntities>> getListSurah() async {
+    final Response<dynamic> surahResponse =
+        await _dio.get('$_baseUrl/${QuranApiEndpoint.surah.name}');
+
+    return List<SurahEntities>.from(
+      surahResponse.data.map(
+        (surah) => SurahEntities.fromJson(surah),
+      ),
+    );
+  }
+
+  Future<DetailSurahEntities> getDetailSurah(int surahNumber) async {
+    final Response<dynamic> detailSurahResponse =
+        await _dio.get('$_baseUrl/${QuranApiEndpoint.surah.name}/$surahNumber');
+    return DetailSurahEntities.fromJson(detailSurahResponse.data);
+  }
+}
