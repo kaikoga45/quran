8b6d39b39245a42876edb6f9b62001bf7333ac33
Kai Koga <koga.kai@icloud.com>
2025-03-26 20:29:00 +0700

    add liver color constant

--- lib/common/constants/color_constant.dart
+++ lib/common/constants/color_constant.dart
@@ -3,4 +3,5 @@ import 'package:flutter/cupertino.dart';
 class ColorConstant {
   static const valentineRed = Color(0xFFE94459);
   static const osloGrey = Color(0xFF8D8D92);
+  static const liver = Color(0xFF4D4D4D);
 }
