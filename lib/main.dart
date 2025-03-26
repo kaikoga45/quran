8728736ffbddef1270598909e979a55c4ee11fc6
Kai Koga <koga.kai@icloud.com>
2025-03-26 14:19:19 +0700

    add multi repository provider, theme data, and route factory

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,39 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quran/common/constants/color_constant.dart';
+import 'package:quran/common/constants/route_constant.dart';
+import 'package:quran/common/helpers/locator/repository_locator_helper.dart';
+import 'package:quran/common/helpers/locator/service_locator_helper.dart';
+import 'package:quran/common/utils/route_factory_util.dart';
+import 'package:quran/core/presentation/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  setupLocator();
+  runApp(const QuranApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QuranApp extends StatelessWidget {
+  const QuranApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MultiRepositoryProvider(
+      providers: repositoriesProvider,
+      child: CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Quran',
+        theme: const CupertinoThemeData(
+          brightness: Brightness.dark,
+          primaryColor: ColorConstant.valentineRed,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        initialRoute: RouteConstant.home,
+        onGenerateRoute: (settings) {
+          return CupertinoPageRoute(
+            builder: (_) => RouteFactoryUtil.getPageByName(settings.name),
+            settings: settings,
+          );
+        },
+        home: const HomePage(),
       ),
     );
   }
